f3f774e6200f8dc3b9e9a94f1deb7192d1c57212
SergeVassal <41280771+SergeVassal@users.noreply.github.com>
2019-03-26 16:40:12 +0200

    Work26.03.

---
+++ LaserGun2019/Assets/Scripts/Environment/PointCloudFactoryGrid.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class PointCloudFactoryGrid : PointCloudFactoryAbstract
+{
+    [SerializeField] private int columnAmount;
+    [SerializeField] private float columnInterval;
+
+    [SerializeField] private int rowAmount;
+    [SerializeField] private float rowInterval;
+
+    [SerializeField] private enum PointCloudDirection
+    {
+        plusX,
+        minusX,
+        plusZ,
+        minusZ
+    }
+
+    private List<Vector3> pointCloud;
+    private PointCloudDirection pointCloudDirection = PointCloudDirection.plusX;
+    private Vector3 initialPos;
+    private Vector3 previousPos;
+    private Vector3 newPosition;
+
+
+    public override List<Vector3> GetPointCloud(Vector3 position)
+    {
+        initialPos = position;
+        CreatePointCloud();
+        return pointCloud;
+    }
+
+    private void CreatePointCloud()
+    {
+        previousPos = initialPos;        
+        pointCloud = new List<Vector3>();
+
+        for(int rowIndex = 0; rowIndex < rowAmount; ++rowIndex)
+        {
+            for(int columnIndex = 0; columnIndex < columnAmount; ++columnIndex)
+            {
+                GetNextPosition(columnIndex,rowIndex);                
+                previousPos = newPosition;
+            }
+            AddNewRow(rowIndex);
+            
+            
+        }
+        for (int i = 0; i < pointCloud.Count; i++)
+        {
+            //Debug.Log(pointCloud[i]);
+        }
+    }
+
+    private void GetNextPosition(int currentColumnIndex,int currentRowIndex)
+    {     
+        
+        AddNewColumn(currentColumnIndex, currentRowIndex);        
+    }
+
+    
+    
+    private void AddNewColumn(int currentColumnIndex, int currentRowIndex)
+    {
+        if (currentColumnIndex == 0 && currentRowIndex == 0)
+        {
+            newPosition = initialPos;
+            pointCloud.Add(newPosition);
+            return;
+        }
+        switch (pointCloudDirection)
+        {
+            case PointCloudDirection.plusX:
+                newPosition.x=previousPos.x + columnInterval;
+                break;
+
+            case PointCloudDirection.minusX:
+                newPosition.x = previousPos.x - columnInterval;
+                break;
+
+            case PointCloudDirection.plusZ:
+                newPosition.z = previousPos.z + columnInterval;
+                break;
+
+            case PointCloudDirection.minusZ:
+                newPosition.z = previousPos.z - columnInterval;
+                break;
+        }
+        pointCloud.Add(newPosition);
+    }
+
+    private void AddNewRow(int rowIndex)
+    {
+        if (rowIndex == 0)
+        {
+            return;
+        }
+        newPosition= new Vector3(initialPos.x, previousPos.y + rowInterval, initialPos.z);
+        pointCloud.Add(newPosition);
+        previousPos = newPosition;
+        Debug.Log(previousPos);
+        Debug.Log(newPosition);
+    }
+
+}
--- LaserGun2019/Assets/WebplayerTemplates/Scripts/Environment/AbstractPointSpawnerFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class AbstractGridPointSpawnerFactory
-{
-    public abstract List<Vector3> CreateGridPointCloud(Vector3 initPosition, int columnAmount, int rowAmount,
-        float columnInterval, float rowInterval, GridPointFactoryDirectionEnum direction);
-}
